a8cc59c2b800eeb8f63dbab0b190ab89808b7d72
Riyan Marayat <riyanmarayat@gmail.com>
2024-04-12 09:18:34 +0700

    12 April 2024, 09.18

---
+++ Day 6 Python Function & Karel/day-6-start.py
@@ -0,0 +1,65 @@
+#Part 60 Defining and Calling Python Functions
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+#Membuat fungsi
+# def my_function():
+#   Do this
+#   Then do this
+#   Finally do this
+
+#Memanggil fungsi
+# my_function()
+
+# def my_function():
+#   print("Hello")
+#   print("bye")
+
+# my_function()
+
+#Part 62 Indentation in Python
+#Indentation
+# def my_function():
+# ....if sky == "clear":
+# ........print("blue")
+# ....elif sky == "cloudy":
+# ........print("grey")
+# ....print("Hello")
+# print("World")
+
+#Part 63 While Loops
+#For
+# for item in list_of_items:
+#   Do something to each item
+
+# for number in range(a, b):
+#   print(number)
+
+#While
+# while something_is_true:
+#   Do something repeatedly
+
+#While Loop
+# While something_is_true:
+#   Do This
+#   Then do this
+#   Then do this
+
+#For
+# for item in list_of_items:
+#   #Do something to each item
+#
+# for number in range(a, b):
+#   print(number)
+
+#While
+# while something_is_true:
+#   #Do something repeatedly
+
+#Infinite Loop
+# while 5 > 3:
+#   #Do this
+#   #Then do this
+#   #Then do this
+
