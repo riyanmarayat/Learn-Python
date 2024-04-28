72da573599af3ef4ad288d2f58f3798b239d1668
Riyan Marayat <riyanmarayat@gmail.com>
2024-04-28 17:10:10 +0700

    28 April 2024, 17.09

---
+++ Day 13 Debugging How to Find and Fix Errors in your Code/day-13-start.py
@@ -0,0 +1,49 @@
+############DEBUGGING#####################
+#Part 124 Describe the Problem
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 21):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+#Part 125 Reproduce the Bug
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
+
+#Part 126 Play Computer and Evaluate Each Line
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year >= 1994:
+#   print("You are a Gen Z.")
+
+#Part 127 Fixing Errors and Watching for Red Underlines
+# # Fix the Errors
+# age = int(input("How old are you?"))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+
+#Part 128 Squash bugs with a print() Statement
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+#Part 129 Bringing out the BIG Gun: Using a Debugger
+# #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#     b_list.append(new_item)
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
