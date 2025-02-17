72209ea9f7cdc6f061c78722949a9492df664b17
Riyan Marayat <riyanmarayat@gmail.com>
2025-02-17 14:08:45 +0700

    17 Februari 2025, 14.08

---
+++ Day 30 Errors, Exceptions and JSON data Improving the Password/day-30/main.py
@@ -0,0 +1,60 @@
+#Part 228 Catching Exceptions: The try catch except finally Pattern
+#FileNotFound
+# with open("a_file.txt") as file:
+#     file.read()
+
+#KeyError
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non_existent_key"]
+
+#IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
+
+#TypeError
+# text = "abc"
+# print(text + 5)
+
+#FileNotFound
+
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"That key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed.")
+
+
+#Part 229 Raising your own Exceptions
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"That key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    raise TypeError('This is an error that I made up.')
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
